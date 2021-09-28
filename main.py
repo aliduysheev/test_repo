a46adc10f5e4bd0d80e71d0f4086429c6753588e
Ali Duysheev <aliduysheev@MacBook-Air-Artur.local>
2021-09-28 20:34:16 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,106 @@
+# dict_= {'TImur':{'h':90, 'm':95, 'l': 91}, 'Vlad': {'m':93, 'v':95, 'l':98}}
+# dict_ = {k1:v1 for k1, v1 in dict_.items() for k2,v2 in v1.item() if max(v1.values()) == v2}
+# print(dict_)
+
+# dict_ = {'first':{'a':1}, 'second': {'b:2'}}
+# dict_ = {k1:v1 for k1, v1 in dict_.items() for k2,v2 in v1.items()}
+# print(dict_)
+
+# try:
+#     age = int(input('введите возраст:'))
+#     if not age in range(18, 65):
+#         raise ValueError('Доступ запрещен далбаеб')
+# except ValueError:
+#     print('Your age must be between 18 and 65')
+# else:
+#     print('OK')
+# finally:
+#     print('Good Bye')
+
+# import random
+# a = (random.randrange(0, 10))
+# while True:
+#     num = input('Guess the number beach:')
+#     print(f'random nuber is {a}')
+
+#     if num.lower() == 'exist':
+#         print('Game over suchka')
+#     elif int(a) > int(num):
+#         print('NUMBER HOLd OOON')
+#     elif int(a) < int(num):
+#         print('NUMBER IS LOWER SHEHEHEHEHEHE')
+#     elif int(a) == int(num):
+#         print('ПОЗДРАВЛЯЮ ДАЛБЕЕБЕБ/nType exit to quit')
+
+# def add_nums(a,b):
+#     return a + b
+# result = add_nums(10, 5)
+# print(result)
+
+# result = lambda a, b: a + b
+# # print(result(10, 5))
+# print(type(add_nums()))
+
+# lst = [1, 2, 4, 6, 9]
+# lst_new = [str(i) for i in lst]
+# for i in lst:
+#     lst_new.append(str(i))
+# print(lst_new)
+# map(function, iterible_obj)
+# def num_to_str(i):
+#     return str(i)
+# lst_new = list(map(lambda i: str(i), lst))
+# lst_new = list(map(num_to_str, lst))
+# print(lst_new)
+
+# filter()
+# filter(func, iterible_obj)
+# lst = [i for i in range(1, 11)]
+# lst = list(range(1, 11))
+# new_lst = list(filter(lambda i: x % 3 != 0, lst))
+# print(new_lst)
+
+# # reduce()
+# from functools import reduce
+# lst = [20, 57, 33]
+# result = reduce(lambda x, y: x / y, lst)
+# print(result)
+
+# print(pow(1, 2))
+
+# # zip()
+# employee_numbers = [2, 4, 56, 14]
+# employee_numes = ['JANArrr', 'ISken', 'MAra', 'SANJIKK']
+# employee_sphere = ['It', 'Finanscompany', 'Cocci', 'ASDDF']
+# zipped_values = zip(employee_numbers, employee_numes, employee_sphere)
+# # print(zipped_values)
+# zipped_list = list(zipped_values)
+# print(zipped_list)from
+
+
+
+# list_of_readers = {'Alex': {'Financial': 'Theodor Draiser'}}
+# list_of_books = {'Financial': 'Theodor Draiser', 'Rich dad, poor dad,': 'robert Kiyosaki', 'Think and get rich': 'Napoleon Hill'}
+# db = {'John': 23, 'Jack': 52, 'Alex': 23, 'Tom': 34}
+# def registration():
+#     global list_of_books
+#     book = input(f'Какую книгу вы хотите хотите получить? Выберите из {list_of_books}')
+#     if book in list_of_books:
+#         print()
+# registration()
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
